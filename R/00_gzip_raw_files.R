942c353b5a31d146232a77fceb5c9ada6ad98b0f
nikopech <nikosp41@gmail.com>
2025-04-22 15:40:15 +0300

    add input datasets

---
+++ R/00_gzip_raw_files.R
@@ -0,0 +1,8 @@
+
+
+
+dir.create("input/tmp")
+
+R.utils::gunzip(filename = "./input/mesh/desc2024_TN.csv.gz", destname = "./input/tmp/desc2024_TN.csv", remove = FALSE)
+R.utils::gunzip(filename = "./input/mesh/MeSH from PMID result_v2.csv.gz", destname = "./input/tmp/MeSH from PMID result_v2.csv", remove = FALSE)
+R.utils::gunzip(filename = "./input/openalex/output_openalex-affil.gz", destname = "./input/tmp/output_openalex-affil", remove = FALSE)
